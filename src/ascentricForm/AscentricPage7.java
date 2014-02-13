0f90752e83558212a09fa1051617f88c8d15c12f
Charlie Brodie <charliembrodie@gmail.com>
2014-02-13 11:53:50 +0000

    For whatever reason I'm now flying through completing these pages and am succeeding in completing tests left, right and center.

--- src/ascentricForm/AscentricPage7.java
+++ src/ascentricForm/AscentricPage7.java
@@ -4,62 +4,62 @@ import java.io.IOException;
 
 import ascentricClientDetails.Client;
 import ascentricClientDetails.ClientHolder;
+import ascentricClientDetails.FinancialAdviserDetails;
 
 import com.itextpdf.text.DocumentException;
 
 public class AscentricPage7 extends AscentricPage {
 
 	private static final int PAGENUMBER = 7;
+	private static final String FIRMNAME = "Master Adviser";
+	private static final String FIRMREF = "593723";
 	
-	public void fillPage(ClientHolder theClient) throws IOException, DocumentException {
+	
+	public void fillPage(Client theClient) throws IOException, DocumentException {
 		setUp(PAGENUMBER);
-		tickFacetoFace();
-		declaration();
+		tickFacetoFace(theClient.getfinancialAdviserDetails().isFaceToFaceContact());
+		declaration(theClient.getfinancialAdviserDetails());
 		shutDown();
 	}
 
-	private void declaration() {
+	private void declaration(FinancialAdviserDetails fad) {
 		int formHeight = 667;
 		int formWidth = 317;
-		String numbers = "12345678";
+		
 		//Firm Name
-		stamp(formWidth, formHeight, "Master Adviser");
+		stamp(formWidth, formHeight, FIRMNAME);
+		
 		//Firm Ref No.
 		int temp = formWidth;
-		stamp(temp, formHeight - 40, "1");
+		stamp(temp, formHeight - 40, "" + FIRMREF.charAt(0));
 		for(int i = 1; i < 6; i++){
-			stamp(temp+=20, formHeight-40, numbers.charAt(i) + "");
+			stamp(temp+=20, formHeight-40, FIRMREF.charAt(i) + "");
 		}
+		
 		//Registered Individual
-		stamp(formWidth, formHeight-84, "Douglas Brodie");
-		//Firm Ref No.
+		stamp(formWidth, formHeight-84, fad.getRegisteredIndividual());
+		
+		//FCA Individual Reference Number
 		temp = formWidth;
-		stamp(temp, formHeight-126, "0");
+		stamp(temp, formHeight-126, fad.getFcaIndividualReferenceNumber().charAt(0) + "");
 		for(int i = 1; i < 8; i++){
-			stamp(temp+=20, formHeight-126, numbers.charAt(i) + "");
+			stamp(temp+=20, formHeight-126, fad.getFcaIndividualReferenceNumber().charAt(i) + "");
 		}
+		
 		//Date
-		String date = "31012014";
 		temp = formWidth+52;
-		stamp(temp, formHeight-206, "0");
-		for(int i = 1; i < date.length(); i++){
+		stamp(temp, formHeight-206, fad.getDate().charAt(0) + "");
+		for(int i = 1; i < fad.getDate().length(); i++){
 			if(i %2 == 0 && i < 5){
-				stamp(temp+=35, formHeight-206, numbers.charAt(i) + "");
+				stamp(temp+=35, formHeight-206, fad.getDate().charAt(i) + "");
 			} else {
-				stamp(temp+=20, formHeight-206, numbers.charAt(i) + "");
+				stamp(temp+=20, formHeight-206, fad.getDate().charAt(i) + "");
 			}
 		}
 	}
 
-	private void tickFacetoFace() {
-		stamp(271, 477, "X");
-	}
-
-	@Override
-	public void fillPage(Client theClient) throws IOException,
-			DocumentException {
-		// TODO Auto-generated method stub
-		
+	private void tickFacetoFace(boolean b) {
+			if(b) stamp(271, 477, "X");
 	}
 
 }
---
+++ src/tests/AscentricPage7Test.java
@@ -0,0 +1,44 @@
+package tests;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.itextpdf.text.DocumentException;
+
+import ascentricClientDetails.Client;
+import ascentricClientDetails.ClientHolder;
+import ascentricClientDetails.FinancialAdviserDetails;
+import ascentricForm.AscentricPage7;
+
+public class AscentricPage7Test {
+	
+	AscentricPage7 thePage = new AscentricPage7();
+	ClientHolder theClientHolder = new ClientHolder();
+	Client theClient;
+	
+	
+	@Before
+	public void setUp(){
+		theClientHolder.makeNewFirstClient();
+		theClient = theClientHolder.getFirstClient();
+		FinancialAdviserDetails fad = theClient.getfinancialAdviserDetails();
+		
+		fillDetails(fad);
+		
+	}
+
+	private void fillDetails(FinancialAdviserDetails fad) {
+		fad.setFcaFirmNumber("839483");
+		fad.setRegisteredIndividual("Douglas Brodie");
+		fad.setFcaIndividualReferenceNumber("38595634");
+		fad.setFaceToFaceContact(true);
+		fad.setDate("25091989");
+	}
+	
+	@Test
+	public void testPage7() throws IOException, DocumentException{
+		thePage.fillPage(theClient);
+	}
+}
