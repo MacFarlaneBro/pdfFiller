32d8c4ffeb12d48cdb3d44cc06014d3c34fca750
Charlie Brodie <charliembrodie@gmail.com>
2014-02-19 16:28:52 +0000

    I Seriously think something is wrong with me at the moment, I have practically no motivation to work for some reason. It's alright up to lunch but afterwards everything just falls apart.
    Right now I've switched away from JavaFX as theres clearly some pretty
    clear runtime bugs in the latest JRE that are messing JFXML up in an
    incredibly frustrating way.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,5 +1,68 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 public class MainWindow {
+	
+	public static void main(String[] args){
+		
+		createGui();
+	}
+	
+	public static void createGui(){
+		JFrame guiFrame = new JFrame();
+		
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("PDF Filler");
+		guiFrame.setSize(300, 250);
+		
+		guiFrame.setLocationRelativeTo(null);
+		
+		String[] fruitOptions = {"Ascentric"};
+		
+		String[] vegOptions = {"Automatic", "Manual"};
+		
+		//The first JPanel contains a JLabel and a JCombobox
+		final JPanel comboPanel = new JPanel();
+		
+		JLabel comboLabel = new JLabel();
+		JComboBox fruits = new JComboBox(fruitOptions);
+		
+		//Adding the Label to the Panel
+		comboPanel.add(comboLabel);
+		comboPanel.add(fruits);
+		
+		final JPanel listPanel = new JPanel();
+		
+		listPanel.setVisible(false);
+		JLabel listLabel = new JLabel("Vegetables");
+		JList vegs = new JList(vegOptions);
+		vegs.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		
+		comboPanel.add(listLabel);
+		comboPanel.add(vegs);
+		
+		JButton vegFruitBut = new JButton("Fill Form");
+		
+		vegFruitBut.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent event){
+				listPanel.setVisible(!listPanel.isVisible());
+				comboPanel.setVisible(!comboPanel.isVisible());
+			}
+
+		});
+		
+		guiFrame.add(comboPanel, BorderLayout.NORTH);
+		guiFrame.add(listPanel, BorderLayout.CENTER);
+		guiFrame.add(vegFruitBut, BorderLayout.SOUTH);
+		
+		guiFrame.setVisible(true);
+		
+	}
 
 }
