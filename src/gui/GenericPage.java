66e68fbe2f292063c3440de5475529fc8cacf217
Charlie Brodie <Charliembrodie@gmail.com>
2014-04-17 10:42:45 +0100

    Adding in new page which provides generic gui methods to Print

---
+++ src/gui/GenericPage.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ascentricClientDetails.ClientHolder;
+import ascentricGui.Page;
+
+public class GenericPage extends Page {
+
+	@Override
+	public void setUp(Stage primaryStage, Scene previousScene,
+			ClientHolder client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void fillAndSaveClientInfo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
