1fe6d94be01eebdbc18694c694dc1eeb9aa38c86
Charlie Brodie <Charliembrodie@gmail.com>
2014-05-08 11:37:58 +0100

    Forgot to add the new class in the last commit

---
+++ src/ascentricGui/CorrespondenceDetails.java
@@ -0,0 +1,35 @@
+package ascentricGui;
+
+
+public class CorrespondenceDetails {
+
+	private String firstAdd;
+	private String secondAdd;
+	private String thirdAdd;
+	private String postCode;
+	public String getFirstAdd() {
+		return firstAdd;
+	}
+	public void setFirstAdd(String firstAdd) {
+		this.firstAdd = firstAdd;
+	}
+	public String getSecondAdd() {
+		return secondAdd;
+	}
+	public void setSecondAdd(String secondAdd) {
+		this.secondAdd = secondAdd;
+	}
+	public String getThirdAdd() {
+		return thirdAdd;
+	}
+	public void setThirdAdd(String thirdAdd) {
+		this.thirdAdd = thirdAdd;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+}
